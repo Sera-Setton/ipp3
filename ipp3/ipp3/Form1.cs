b8e83dea4d171cecc43d7ba6e7cc2f6abf97c114
Ssskel <72123357+Sera-Setton@users.noreply.github.com>
2023-05-23 19:40:08 +0300

    Add files via upload

---
+++ ipp3/ipp3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ipp3
+{
+    public partial class Form1 : Form
+    {
+        private const int Port = 8;
+        private const string ServerIP = "127.0.0.1";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient();
+
+            try
+            {
+                client.Connect(ServerIP, Port);
+                NetworkStream networkStream = client.GetStream();
+
+                string message = textBox1.Text;
+
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                networkStream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                MessageBox.Show(response);
+
+                networkStream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при з'єднанні з сервером: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ipp3/server/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        private const int Port = 8;
+        private TcpListener listener;
+        private Thread listenThread;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, Port);
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+            UpdateStatusLabel("Сервер запущений. Очікування з'єднання...");
+        }
+
+        private void ListenForClients()
+        {
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientCommunication(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            UpdateStatusLabel("Отримано повідомлення від клієнта: " + message);
+
+            // Виконати дії згідно отриманого повідомлення
+            // Наприклад, малювати фігуру або виконати іншу операцію
+
+            byte[] response = Encoding.ASCII.GetBytes("Повідомлення отримано на сервері.");
+            networkStream.Write(response, 0, response.Length);
+
+            networkStream.Close();
+            tcpClient.Close();
+        }
+
+        private void UpdateStatusLabel(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateStatusLabel), message);
+            }
+            else
+            {
+                label1.Text = message;
+            }
+        }
+
+        private void ServerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listener.Stop();
+            listenThread.Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
